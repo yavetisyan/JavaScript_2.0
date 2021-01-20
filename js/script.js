340764b055a7f6ae6bc8b2db42ccb93a131e8d4e
yavetisyan <y.avetisyan88@gmail.com>
2021-01-20 23:00:36 +0400

    Стартуем и пишем первую программу

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// let a = 'Иван';
+// let b = '37';
+// let z7 = 'Овен';
+// let c = ['Иван', 37, 'Овен'];  //array
+// let d = [];
+
+// console.log(c[0]);
+// console.log(c[1]);
+// console.log(c[5]);
+// console.log(c);
+// console.log(c.length);
+
+// console.info('Hello');
+
+// ---------------
+
+// document.getElementById('out').innerHTML = "Hello";
+// document.getElementById('out').innerHTML = 2021;
+
+// document.getElementById('out').innerHTML = '<b>Avetisyan</b>';
+// document.querySelector('h2').innerHTML = 221234;
+// document.querySelector('.header').innerHTML = 'mi ban ';
+
+// document.querySelector('#one').innerHTML = 'New Text';
+
+let header;
+let one = document.querySelector('#one');
+header = document.querySelector('.header');
+
+header.innerHTML = 'New test add';
+one.innerHTML = 'Changed paragraph';
+
+
+let name = document.querySelector('#name');
+let age = document.querySelector('#age');
